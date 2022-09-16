8e5b832297abe82662e54ece3cd7751b085dfd93
Zak <zakaria.saighi@gmail.com>
2022-09-16 09:26:47 +0200

    Set up sessions and chatroom

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'chatroom#index'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'login', to: 'sessions#new'
+
 end
