d5c97d4e658bc8eb1ebfa13a408bbca618c0ca29
Zak <zakaria.saighi@gmail.com>
2022-09-16 09:55:08 +0200

    Adding Semantic UI SCSS

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,4 +4,9 @@ pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+
+# Pin jquery
+pin "jquery", to: "https://code.jquery.com/jquery-3.6.1.min.js", preload: true
+pin "semantic-ui", to: "https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.js"
+
 pin_all_from "app/javascript/controllers", under: "controllers"
