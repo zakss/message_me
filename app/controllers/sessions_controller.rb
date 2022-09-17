cabebc78a4bb2263b367be780cfc90ca292ff412
Zak <zakaria.saighi@gmail.com>
2022-09-17 20:43:55 +0200

    Create and destroy Session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,24 @@
 class SessionsController < ApplicationController
 
+
   def create
-    user = User.find_by(username: params[:session][:username].downcase)
+    user = User.find_by(username: params[:session][:username])
     if user && user.authenticate(params[:session][:password])
-      redirect_to user
+      session[:user_id] = user.id
+
+      flash[:notice] = "Logged in successfully "
+      redirect_to root_path
     else
-      render 'new'
+      flash.now[:alert] = "There was something wrong"
+      render :new, status: :unprocessable_entity
     end
   end
 
   def destroy
+    session[:user_id] = nil
+
+    flash[:notice] = "Logged out"
+    redirect_to login_path
   end
 
   def new
